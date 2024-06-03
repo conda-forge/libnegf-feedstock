9a37b3e1e356fcd21a146c4770cb3e7b57b7d001
Min RK <benjaminrk@gmail.com>
2024-06-03 14:22:10 +0200

    update env for openmpi 5

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,11 @@ fi
 
 if [ "${mpi}" == "openmpi" ]; then
   export OPAL_PREFIX=$PREFIX
-  export OMPI_MCA_plm=isolated
-  export OMPI_MCA_btl_vader_single_copy_mechanism=none
-  export OMPI_MCA_rmaps_base_oversubscribe=yes
+  export OMPI_MCA_plm_ssh_agent=false
+  export OMPI_MCA_pml=ob1
+  export OMPI_MCA_mpi_yield_when_idle=true
+  export OMPI_MCA_btl_base_warn_component_unused=false
+  export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe
 fi
 
 cmake_options=(
